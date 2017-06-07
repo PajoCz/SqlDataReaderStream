76040f90acfe65ee6aebefacdd58b8d35e9ac2a7
PajoPajo <pajo@atlas.cz>
2017-06-07 14:25:16 +0200

    TypeWithConverter

---
+++ SqlDataReaderStream/SqlDataReaderStream/TypeWithConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SqlDataReaderStream
+{
+    public class TypeWithConverter
+    {
+        public TypeWithConverter(Type p_Type, TypeConverter p_TypeConverter)
+        {
+            Type = p_Type;
+            TypeConverter = p_TypeConverter;
+        }
+
+        public Type Type { get; set; }
+        public TypeConverter TypeConverter { get; set; }
+    }
+}
