cc4ad95e767c3e570a590299cd5e82da6a713b53
PajoPajo <pajo@atlas.cz>
2017-06-12 12:01:50 +0200

    v1.5

--- SqlDataReaderStream/SqlDataReaderStream.D3Test/Properties/AssemblyInfo.cs
+++ SqlDataReaderStream/SqlDataReaderStream.D3Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlDataReaderStream")]
+[assembly: AssemblyTitle("SqlDataReaderStream.D3Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlDataReaderStream")]
-[assembly: AssemblyCopyright("Copyright © pbalas 2017")]
+[assembly: AssemblyProduct("SqlDataReaderStream.D3Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("40b6505e-805b-4cf6-bd84-338d8187e2c2")]
+[assembly: Guid("4d73840b-2d34-49e7-92d0-6d3ac3a91a7f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SqlDataReaderStream/SqlDataReaderStream.D3Test/StreamToDataTableRowsTest.cs
@@ -0,0 +1,94 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using NUnit.Framework;
+using SqlDataReaderStream.Serializer;
+
+namespace SqlDataReaderStream.D3Test
+{
+    [TestFixture]
+    public class StreamToDataTableRowsTest
+    {
+        private static SqlConnection CreateConnection()
+        {
+            var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString);
+            conn.Open();
+            return conn;
+        }
+        
+        private void TestSqlValueSerializerForReadingAndWriting(ISqlValueSerializer p_SqlValueSerializer, int p_BufferSize)
+        {
+            var sql = @"
+DECLARE @p0 dbo.tt_ReportManagerSelectList
+INSERT INTO @p0 VALUES('CisloPozadavku','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('Mena','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('Tarif','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('Sezona','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('CK','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('CKa','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('P1','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('KrNC','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('PNC','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('KM','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('KC','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('PM','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('PC','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('SpotrebaMWh','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('VaR','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('VaRZaMWh','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('ZdaAkceptovany','Neagregovan','','False','9')
+INSERT INTO @p0 VALUES('PocetDnu','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('Poznamka','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('IdOceneni','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('Produkt','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('PozadavekCilOceneni','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('IdDiagram_Template','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('IdDiagramBurza_Template','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('IdOceneniCenovyElement','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('IdOceneniStav','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('IdPozadavek','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('Obor','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('IdOceneniDiagram','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('IdOceneniTyp','Neagregovan','','False','1')
+INSERT INTO @p0 VALUES('WorkflowInstanceState','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('WorkflowProcesState','Neagregovan','','False','0')
+INSERT INTO @p0 VALUES('ViditelnostDleStavuOceneni','Neagregovan','','False','9')
+INSERT INTO @p0 VALUES('IdPozadavekTypNakupu','Neagregovan','','False','1')
+DECLARE @p1 dbo.tt_ReportManagerFilterList
+INSERT INTO @p1 VALUES('o.IdOceneniStav','JeVSeznamu','7','','0')
+INSERT INTO @p1 VALUES('ob.IdObor','JeVSeznamu','2','','0')
+DECLARE @p2 dbo.tt_ReportManagerOrderBy
+INSERT INTO @p2 VALUES('IdOceneni','Sestupne')
+INSERT INTO @p2 VALUES('Mena','Vzestupne')
+EXECUTE Oept.p_rd_OceneniAkceptace @SelectList=@p0,@FilterList=@p1,@OrderBy=@p2,@SelectTop=10000,@Aggregation=0,@UserName='rmacejik'
+";
+            var cmd = new SqlCommand(sql, CreateConnection());;
+
+            //ACT with ASSERT
+            using (var sqlDataReaderStream = new SqlStream(cmd, p_SqlValueSerializer))
+            {
+                var table = sqlDataReaderStream.DataTableWithoutData;
+                Assert.AreEqual(0, table.Rows.Count);
+                new StreamToDataTableRows().ReadStreamToDataTable(sqlDataReaderStream, table, p_SqlValueSerializer, p_BufferSize);
+                Assert.AreEqual(6194, table.Rows.Count);
+            }
+        }
+
+        [Test]
+        public void ReadStreamToDataTable_SqlValueSerializerCsvSimple_Buffer80kB()
+        {
+            TestSqlValueSerializerForReadingAndWriting(new SqlValueSerializerCsvSimple(), 80 * 1024);
+        }
+
+        [Test]
+        public void ReadStreamToDataTable_SqlValueSerializerCsvSimple_Buffer8kB()
+        {
+            TestSqlValueSerializerForReadingAndWriting(new SqlValueSerializerCsvSimple(), 8 * 1024);
+        }
+
+        [Test]
+        public void ReadStreamToDataTable_SqlValueSerializerCsvSimple_Buffer1kB()
+        {
+            TestSqlValueSerializerForReadingAndWriting(new SqlValueSerializerCsvSimple(), 1 * 1024);
+        }
+    }
+}
